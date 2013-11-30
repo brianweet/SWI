c569a85e9d02309f98a241ec0bd9ab4965b48cee
brianweet <brian_weeteling@hotmail.com>
2013-11-30 15:54:36 +0100

    commented functions in live_fn

--- Live_fn.m
+++ Live_fn.m
@@ -1,6 +1,7 @@
 
 function fposition=Live_fn(x)
 
+% % Original live_fn
 %     p=0;q=0;
 %     for k=1:5
 %         p=p+k*cos((k+1)*x(1)+k);
@@ -9,10 +10,13 @@ function fposition=Live_fn(x)
 % 
 % fposition=p*q+(x(1)+1.42513)^2+(x(2)+.80032)^2;
 
+% Custom: Sphere function
 fposition = sum(x.^2);
 
+% % Custom: Rosenbrock function
 % fposition = 100 * sum((x(1:end-1).^2 - x(2:end)).^2) + sum((x(1:end-1)-1).^2);
 
+% % Custom: ackley function
 % [ps,n] = size(x);
 %   fposition = 20 - 20 * exp(-0.2 * sqrt(sum(x .^ 2, 2) / n)) ...
 %         - exp(sum(cos(2 * pi * x), 2) / n) + exp(1);
